8b072fb9483568028a27f42a7710f146ffea2c8a
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2024-11-11 17:34:28 +0300

    Simple Using classes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,42 @@
+import {Component} from "react";
 import './App.css';
-import ReactDOM from "react-dom/client";
-import React from 'react';
 
-function App() {
-    class Link extends React.Component {
-        render() {
-            return React.createElement(
-                'p',
-                null,
-                React.createElement(
-                    'a',
-                    {href: this.props.url},
-                    this.props.children));
-        }
+class MenuItem extends Component {
+    render() {
+        return (
+            <li className="menu-item">
+                <a className="menu-link" href={this.props.href}>
+                    {this.props.label}
+                </a>
+            </li>
+        )
     }
-
-    const boldReact =
-        React.createElement('strong', null, 'React');
-
-    const linkReact =
-        React.createElement(Link, {url: '//react.dev'}, boldReact);
-    const linkVue =
-        React.createElement(Link, {url: '//vuejs.org'}, 'Vue');
-    const linkAngular =
-        React.createElement(Link, {url: '//angular.io'}, 'Angular');
-    const group = React.createElement(React.Fragment, null, linkReact, linkVue, linkAngular);
-    const domElement = document.getElementById('root');
-    ReactDOM.createRoot(domElement).render(group);
 }
+
+class Menu extends Component {
+    render() {
+        return (
+            <nav className="navbar">
+                <h1 className="title">Меню компании</h1>
+                <ul className="menu">
+                    <MenuItem label="Главная" href="/"/>
+                    <MenuItem label="О нас" href="/about"/>
+                    <MenuItem label="Блог" href="/blog"/>
+                </ul>
+            </nav>
+        )
+    }
+}
+
+class App extends Component {
+    render() {
+        return (
+            <main>
+                <Menu/>
+            </main>
+        );
+    }
+}
+
 
 export default App;
